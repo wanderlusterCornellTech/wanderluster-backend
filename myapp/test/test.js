1c549ca2c2170980e521855a13025d6e4c109bad
liudan30 <lx98@cornell.edu>
2015-12-07 15:08:02 -0500

    test ci

--- myapp/test/test.js
+++ myapp/test/test.js
@@ -3,21 +3,66 @@ var should = require("should");
 
 describe("SAMPLE unit test",function(){
 
-var url = 'http://localhost:80';
-  // #1 should return home page
+		var url = 'http://localhost:80';
+		// #1 should return home page
+		var body = {};
+		it('Login Success', function(done){
+				request(url)
+				.get('/api/login/lx123/123456')
+				.send(body)
+				.expect(200) //Status code
+				.end(function(err,res) {
+						if (err) {
+						throw err;
+						}
+						// Should.js fluent syntax applied
+						res.body.message.should.equal('Login Success!')
+						done();
+						});
+				});
 
-  it('should get user', function(done){
-            request(url)
-                .get('/api')
-                //.send(body)
-                .expect(200) //Status code
-                .end(function(err,res) {
-                    if (err) {
-                        throw err;
-                    }
-                    // Should.js fluent syntax applied
-                    res.status.should.equal(200);
-                    done();
-                });
-        });
+		it('Login Fail', function(done){
+				request(url)
+				.get('/api/login/shimiao123/654321')
+				.send(body)
+				.expect(200) //Status code
+				.end(function(err,res) {
+						if (err) {
+						throw err;
+						}
+						res.body.message.should.equal('Username or Password invalid')
+                                                done();
+						});
+				});
+
+		it('Register Success', function(done) {
+				request(url)
+				.post('/api/register/abcd/123456')
+				.send(body)
+				.expect(200)
+				.end(function(err,res) {
+					if (err) {
+					throw err;
+					}
+					res.body.message.should.equal('Success!')
+					done();
+					});
+			});
+		
+		it('Register Fail', function(done){
+				request(url)
+				.post('/api/register/lx123/123456')
+				.send(body)
+				.expect(200) //Status code
+				.end(function(err,res) {
+						if (err) {
+						throw err;
+						}
+						// Should.js fluent syntax applied
+						res.body.message.should.equal('Username has been used')
+						done();
+						});
+				});
+			
+
 });
